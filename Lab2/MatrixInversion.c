5fe7cdbe10c78d26eef269eac76cf05a9bdf61b3
Anna Bieber <anna.bieber@mail.mcgill.ca>
2019-11-01 16:11:33 -0400

    working with imported matrix

--- Lab2/MatrixInversion.c
+++ Lab2/MatrixInversion.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include "b_32.h"
+#include "a_32.h"
+
 
 #define mat_elem(a, y, x, n) (a + ((y) * (n) + (x)))
-#define N 6
+#define N 32
 
 //input full arrays a and b, diagonale value, the max_row and size n
-void swap_row(double* a, double* b, int r1, int r2)
+void swap_row(float* a, float* b, int r1, int r2)
 {
 	//create variables for later
-	double tmp, * p1, * p2;
-	int i;
+	float tmp, * p1, * p2;
 
 	//if the diagonal is the max then we're not swapping cause they will be the same
 	if (r1 == r2) return;
 	//swap the elements of each row 
-	for (i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++) {
 		p1 = mat_elem(a, r1, i, N);
 		p2 = mat_elem(a, r2, i, N);
 		tmp = *p1, * p1 = *p2, * p2 = tmp;
@@ -23,7 +25,7 @@ void swap_row(double* a, double* b, int r1, int r2)
 	tmp = b[r1], b[r1] = b[r2], b[r2] = tmp;
 }
 
-void matrix_multiplication(double a[N][N], double b[], double* x) {
+void matrix_multiplication(float a[N][N], float b[], float* x) {
 	double temp;
 	//matrix multiplication -- need to parallelize
 	for (int row = N - 1; row >= 0; row--) {
@@ -39,11 +41,11 @@ void matrix_multiplication(double a[N][N], double b[], double* x) {
 }
 
 //input two full arrays a and b, one empty array x and size of x and b
-void gauss_eliminate(double a[N][N], double b[], double* x)
+void gauss_eliminate(float a[][N], float b[], float* x)
 {
 	//create variables for later
 	int i, j, col, row, max_row, dia;
-	double max, tmp;
+	float max, tmp;
 
 	//iterate over the matrix diagonaly, n is the size of x and y
 	//iterate by row/diagonal
@@ -62,7 +64,7 @@ void gauss_eliminate(double a[N][N], double b[], double* x)
 				max_row = row, max = tmp;
 
 		//swap rows - see method above
-		swap_row(a, b, dia, max_row, N);
+		swap_row(*a, b, dia, max_row);
 
 		//iterate over the rows again 
 		for (row = dia + 1; row < N; row++) {
@@ -79,24 +81,19 @@ void gauss_eliminate(double a[N][N], double b[], double* x)
 	}
 
 	//where we need to use threads
-	matrix_multiplication(a, b, x, N);
+	matrix_multiplication(a, b, x);
 }
 
 int main(void)
 {
-	double a[N][N] = {
-		{1.00, 0.00, 0.00,  0.00,  0.00, 0.00},
-	{1.00, 0.63, 0.39,  0.25,  0.16, 0.10},
-	{1.00, 1.26, 1.58,  1.98,  2.49, 3.13},
-	{1.00, 1.88, 3.55,  6.70, 12.62, 23.80},
-	{1.00, 2.51, 6.32, 15.88, 39.90, 100.28},
-	{1.00, 3.14, 9.87, 31.01, 97.41, 306.02}
-	};
-	double b[N] = { -0.01, 0.61, 0.91, 0.99, 0.60, 0.02 };
-	double x[N];
+	//float a[N][N] = { {1, 2}, {18, 20} };
+	//float b[N] = {4, 5};
+	float x[N];
 	int i;
 
-	gauss_eliminate(a, b, x);
+	
+	gauss_eliminate(A_32, b_32, x);
+	//gauss_eliminate(a, b, x);
 
 	for (i = 0; i < N; i++)
 		printf("%g\n", x[i]);
